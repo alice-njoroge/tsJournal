fd14ebdfc832b67a9ef3059effea5c0a31f8f2e3
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-03-06 21:06:16 +0300

    ts and computed properties

--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import EmojiField from "@/components/EmojiField.vue";
 import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import type emojis from "@/Types/emojis";
 
 const props = defineProps<{
@@ -13,18 +13,16 @@ const props = defineProps<{
 
 const textInput = ref("");
 const emoji = ref<emojis | null>(null);
+const charCount = computed<number>(() => textInput.value.length);
 
-const selectedEmoji = () => {
-  console.log("selected", emoji.value);
-};
 
 </script>
 <template>
   <form class="entry-form" @submit.prevent>
     <textarea v-model="textInput" :placeholder="`New Journal Entry for ${props?.user?.name}`"></textarea>
-    <EmojiField @update:model-value="selectedEmoji" v-model="emoji" />
+    <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>0 / 280</span>
+      <span> {{ charCount }} / 280</span>
       <button>Remember
         <ArrowCircleRight width="20" />
       </button>
