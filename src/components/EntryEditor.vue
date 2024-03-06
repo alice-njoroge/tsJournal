591295ded39f2aeb80deb0099494bb9c520c0d72
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-03-06 21:40:02 +0300

    ts and events

--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -15,11 +15,19 @@ const textInput = ref("");
 const emoji = ref<emojis | null>(null);
 const charCount = computed<number>(() => textInput.value.length);
 
+const handleChange = (event: Event) => {
+  console.log(event?.target?.value);
+if (textInput.value.length > 20){
+  textInput.value = textInput.value.substring(0,20)
+}
+};
+
 
 </script>
 <template>
   <form class="entry-form" @submit.prevent>
-    <textarea v-model="textInput" :placeholder="`New Journal Entry for ${props?.user?.name}`"></textarea>
+    <textarea @keyup="handleChange" v-model="textInput"
+              :placeholder="`New Journal Entry for ${props?.user?.name}`"></textarea>
     <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
       <span> {{ charCount }} / 280</span>
